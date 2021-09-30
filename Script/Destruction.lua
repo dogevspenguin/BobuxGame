e008361fa638a7f6833ae2d1b580ff45911a0eb6
Cukkoo <62637922+dogevspenguin@users.noreply.github.com>
2021-09-30 21:10:11 +0700

    e

--- Script/Destruction.lua
+++ Script/Destruction.lua
@@ -5,10 +5,12 @@ script.Parent.Touched:Connect(function(hit)
 	if hit:FindFirstChild("Hard") and debounce == false then
 		debounce = true
 		local value = hit:FindFirstChild("Hard")
-		if value.Value == 1 then
+		if value.Value >= 1 then
 			value.Value = value.Value - 1
+			print(value.Value)
 		else
 			value.Parent:Destroy()
+			print("Destroyed")
 		end
 		wait(1)
 		debounce = false
